c233d247944293a25b5d60a7e3ece2c70822bcfe
flyingbuta <1073971990@qq.com>
2024-06-17 11:02:51 +0800

    初识项目创建

---
+++ src/main/java/com/project/springbootprojectbackend/SpringbootProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.springbootprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springbootprojectbackend/SpringbootProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springbootprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
